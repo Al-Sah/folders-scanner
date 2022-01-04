f8f8b81317beb76e9ca285fbb36320e04b1361f9
Al_Sah <aplexidr@gmail.com>
2022-01-04 18:18:44 +0200

    Implement users search

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,15 +1,51 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Windows.Forms;
 
 namespace FolderScanner
 {
     public partial class MainWindow : Form
     {
-        private string _root = @"C:\Users\";
+        private const string _root = @"C:\Users\";
+        private bool _findAllUsers = false;
+
+        private List<User> _users;
         public MainWindow()
         {
             InitializeComponent();
+
+            _users = GetUsers();
         }
 
+        private List<User> GetUsers()
+        {
+            // Get All users using WMI
+            var allUsers = new ManagementObjectSearcher(@"SELECT * FROM Win32_UserAccount").Get();
+            
+            var filteredUsers = allUsers.Cast<ManagementObject>().Where( user =>
+            {
+                if (_findAllUsers)
+                {
+                    return (bool) user["LocalAccount"] && int.Parse(user["SIDType"].ToString()) == 1;
+                }
+                return (bool) user["LocalAccount"] && // Account is defined on the local computer.
+                    !(bool)user["Disabled"] &&     // Windows user account is not disabled.
+                    !(bool)user["Lockout"]  &&     // User account is not locked out of the Windows operating system.
+                    int.Parse(user["SIDType"].ToString()) == 1; // Type is 'SidTypeUser'
+            });
+
+            return filteredUsers.Select( user => 
+                new User(user, FindUserHomeDir(user["Name"].ToString())))
+                .ToList();
+        }
+
+        private static string FindUserHomeDir(string user)
+        {
+            // TODO other locations ??
+            return Directory.Exists(_root + user) ? _root + user : string.Empty;
+        }
 
     }
 }
---
+++ User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Management;
+
+namespace FolderScanner
+{
+    public class User
+    {
+        public bool Disabled { get; }
+        public bool Lockout;
+        
+        public string Domain;
+        public string Name;
+        public string Caption;
+
+        public string HomeDirectory;
+
+        public User(ManagementObject user, string home)
+        {
+            Disabled = (bool) user["Disabled"];
+            Lockout = (bool) user["Lockout"];
+            
+            Domain = user["Domain"].ToString();
+            Name = user["Name"].ToString();
+            Caption = user["Caption"].ToString();
+            
+            HomeDirectory = home;
+        }
+        
+    }
+}
