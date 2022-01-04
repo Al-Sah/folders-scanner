70a08ebaa2b6be37c4fbce61f587892c6a7b2267
Al_Sah <aplexidr@gmail.com>
2022-01-04 17:08:20 +0200

    Init project

---
+++ MainWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace FolderScanner
+{
+    public partial class MainWindow : Form
+    {
+        private string _root = @"C:\Users\";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
